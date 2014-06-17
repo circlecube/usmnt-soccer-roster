c8b7b6b1fc027179f29ecd7a725d7601148abcd6
Evan Mullins <evanrm@gmail.com>
2014-06-17 10:42:08 -0400

    updating caps in roster from Ghana game

--- js/roster.js
+++ js/roster.js
@@ -27,7 +27,7 @@ var usmnt_players = [
 	 'img_pos': 'center center',
 	 'img2': '051414-USMNT-30-man-Preliminary-Roster-Sunderland-Jozy-Altidore.jpg',
 	 'img2_pos':'70% center',
-	 'caps': 70,
+	 'caps': 71,
 	 'goals': 23,
 	 'number': 17,
 	 'bio':"Started all four American games at 2010 World Cup. In 2013 became the first American to score in five consecutive international matches. Played in all three U.S. games during the 2008 Olympics. Made his professional debut at 16 with the New York Reds Bulls in 2006. Joined Villarreal in 2008, was loaned to Xerez (2009), Hull (2009-10) and Busaspor (2011), then sold to AZ Alkmaar for the 2011-12 season. Scored 19 goals in his first season in the Netherlands and 31 in his second, breaking Dempsey's record for goals by an American in a European club season. Joined Sunderland for 2013-14 and scored two goals in 39 games, reminiscent of 2009-10, when he had two goals in 30 games for Hull."
@@ -43,7 +43,7 @@ var usmnt_players = [
 	 'img_pos': '75% center',
 	 'img2': '051414-USMNT-30-man-Preliminary-Roster-Puebla-DaMarcus-Beasley.jpg',
 	 'img2_pos':'center center',
-	 'caps': 116,
+	 'caps': 117,
 	 'goals': 17,
 	 'number': 7,
 	 'bio':"A midfielder for most of his career, has been moved to left back by the national team in recent years. Was youngest member of 2002 U.S. World Cup team. Could become the first American to appear in four World Cups. Has played for Chicago (2000-04), PSV Eindhoven (2004-07), Manchester City (2006-07), Glasgow Rangers (2007-10), Hannover (2010-11) and Puebla (2011-present). In 2005 became the first American to appear in a Champions League semifinal, starting and playing until the 61st minute for PSV Eindhoven against AC Milan. Won Silver Ball as the U.S. finished fourth at the 1999 FIFA World Under-17 Championship, where Landon Donovan won the Golden Ball."
@@ -59,7 +59,7 @@ var usmnt_players = [
 	 'img_pos': '60% center',
 	 'img2': '051414-USMNT-30-man-Preliminary-Roster-Real-Salt-Lake-Kyle-Beckerman.jpg',
 	 'img2_pos':'center center',
-	 'caps': 37,
+	 'caps': 38,
 	 'goals': 1,
 	 'number': 15,
 	 'bio':"Scored against Panama in quarterfinal of 2009 CONCACAF Gold Cup. Was teammate of Donovan and Beasley as U.S. finished fourth at the 1999 FIFA Under-17 World Championship. Has spent entire career in MLS with Miami (2000-01), Colorado (2002-07) and Salt Lake, where he has been captain since 2008."
@@ -75,7 +75,7 @@ var usmnt_players = [
 	 'img_pos': '25% center',
 	 'img2': '051414-USMNT-30-man-Preliminary-Roster-Nantes-Alejandro-Bedoya.jpg',
 	 'img2_pos':'center center',
-	 'caps': 28,
+	 'caps': 29,
 	 'goals': 1,
 	 'number': 11,
 	 'bio':"Scored against Guatemala in exhibition last July ahead of CONCACAF Gold Cup. Was on 30-man preliminary roster for 2010 World Cup, then was cut. After playing at Fairleigh Dickinson (2005-06) and Boston College (2007-08), has spent professional career in Europe with Orebro (2009-11), Glasgow Rangers (2011-12), Helsingborgs (2012-13) and Nantes (2013-present)."
@@ -91,7 +91,7 @@ var usmnt_players = [
 	 'img_pos': '58% center',
 	 'img2': '051414-USMNT-30-man-Preliminary-Roster-Sporting-Kanas-City-Matt-Besler.jpg',
 	 'img2_pos':'40% center',
-	 'caps': 17,
+	 'caps': 18,
 	 'goals': 0,
 	 'number': 5,
 	 'bio':"Made national team debut against Canada in January 2013 and wound up starting six games in the final round of World Cup qualifying. Was Notre Dame's captain as junior and senior in 2007 and '08."
@@ -107,7 +107,7 @@ var usmnt_players = [
 	 'img_pos': '75% center',
 	 'img2': '051314-Soccer-USA-Michael-Bradley.jpg',
 	 'img2_pos':'60% center',
-	 'caps': 86,
+	 'caps': 87,
 	 'goals': 12,
 	 'number': 4,
 	 'bio':"Son of former U.S. coach Bob Bradley. Trained with U.S. World Cup team in 2006 and made his national team appearances that May in exhibition against Venezuela. Scored against tying goal vs. Slovenia in 2-2 draw during 2010 World Cup. Had two goals against Mexico in 2009 World Cup qualifier. Received red card vs. Spain in 2009 Confederations Cup semifinal, causing him to miss final. Played twice at the 2008 Olympics. Began pro career with New York-New Jersey MetroStars (2004-05), and has gone on to play for Heerenveen (2006-08), Borussia Moenchengladbach (2008-11), Aston Villa (2011), Chievo Verona (2011-12) and Roma (2012-14). Signed in January with Toronto."
@@ -123,8 +123,8 @@ var usmnt_players = [
 	 'img_pos': 'center center',
 	 'img2': '051314-Soccer-USA-John-Brooks.jpg',
 	 'img2_pos':'20% center',
-	 'caps': 4,
-	 'goals': 0,
+	 'caps': 6,
+	 'goals': 1,
 	 'number': 6,
 	 'bio':"Made national team debut in August at Bosnia-Herzegovina. The son of an American serviceman, was raised in Berlin and visited family in Chicago. Made debut with Hertha Berlin in 2012-13, helping earn promotion to the Bundesliga, and scored in his Bundesliga debut against Eintracht Frankfurt last Aug. 10. Missed time this season with broken elbow and strained knee ligament. Caused a commotion in April when he got a back tattoo, causing him to be benched for a club game."
 	},
@@ -139,7 +139,7 @@ var usmnt_players = [
 	 'img_pos': 'center center',
 	 'img2': '051414-USMNT-30-man-Preliminary-Roster-Stoke-City-Geoff-Cameron.jpg',
 	 'img2_pos':'30% center',
-	 'caps': 27,
+	 'caps': 28,
 	 'goals': 1,
 	 'number': 20,
 	 'bio':"Scored on May 29 last year in 4-2 exhibition loss to Belgium. After playing college ball at West Virginia and Rhode Island, spent 2008-12 with the Houston Dynamo and then transferred to Stoke. Plays mostly right back for his club, but preferred at center back with the national team. Scored first Premier League goal against Arsenal in September, then got another when he beat fellow American Brad Guzan during March victory over Aston Villa."
@@ -187,8 +187,8 @@ var usmnt_players = [
 	 'img_pos': 'center center',
 	 'img2': '051314-Soccer-Seattle-Sounders-FC-Clint-Dempsey.jpg',
 	 'img2_pos':'center center',
-	 'caps': 105,
-	 'goals': 37,
+	 'caps': 106,
+	 'goals': 38,
 	 'number': 8,
 	 'bio':"Scored vs. Ghana in 2006 World Cup and vs. England in 2010 World Cup. Scored vs. Egypt, Spain and Brazil at 2009 Confederations Cup, where he earned Bronze Ball as third-best player. Played for New England (2004-06), Fulham (2007-12, 2014), Tottenham (2012-13) and Seattle (2013-present). Scored 60 goals for Cottagers, including 23 in 2011-12, then a record for an American with a European club. Goal on long chip beat Juventus and advance Fulham to the 2009-10 Europa League quarterfinals. Played in overtime loss to Atletico Madrid in the final. Scored three goals vs. Newcastle on Jan. 21, 2012, becoming the first American to score a hat trick in England's Premier League. Fulham player of the year in 2010-11 and 2011-12. Nickhamed \"Deuce,\" his number with Tottenham and Seattle. Recorded rap song as part of 2006 World Cup marketing promotion."
 	},
@@ -267,7 +267,7 @@ var usmnt_players = [
 	 'img_pos': 'center center',
 	 'img2': '051414-USMNT-30-man-Preliminary-Roster-Everton-Tim-Howard.jpg',
 	 'img2_pos':'center center',
-	 'caps': 100,
+	 'caps': 101,
 	 'goals': 0,
 	 'number': 1,
 	 'bio':"Has spent the past eight seasons as the starter with Everton in England's Premier League after six years with the New York/New Jersey MetroStars and three seasons with Manchester United. Had 15 shutouts, one behind co-leaders Petr Cech of Chelsea and Wojciech Szczesny of Arsenal. On Jan. 4, 2012, scored on a 100-yard-plus, wind-blown clearance against Bolton's Adam Bogdan, becoming the fourth goalkeeper to score in Premier League history. Had 3-0 shutout of Millwall in the 2004 FA Cup final and was voted Premier League goalkeeper of the year. Was a backup to Kasey Keller at the 2006 World Cup and the U.S. starter at the 2010 Cup. Holds U.S. record with 54 wins."
@@ -283,7 +283,7 @@ var usmnt_players = [
 	 'img_pos': 'center center',
 	 'img2': '051414-USMNT-30-man-Preliminary-Roster-AZ-Alkmaar-Aron-Johannsson.jpg',
 	 'img2_pos':'10% center',
-	 'caps': 8,
+	 'caps': 9,
 	 'goals': 2,
 	 'number': 9,
 	 'bio':"Born in Alabama to Icelandic parents and moved to Iceland when he was 3. Attended the IMG Academy in Bradenton, Florida, in 2007-08. Appeared in eight qualifiers for Iceland for the 2012 European Under-21 Championship, then asked FIFA for a change in affiliation last summer. Made U.S. debut in August and scored second-half, stoppage-time goal for 3-2 win at Panama in World Cup qualifier last October, then scored against Azerbaijan in May 2014. Played for Fjolnir in Iceland from 2008-10, Aarhus in Denmark (2010-13) and AZ Alkmaar in the Netherlands (2013-present), scoring 26 goals this season."
@@ -299,7 +299,7 @@ var usmnt_players = [
 	 'img_pos': '40% center',
 	 'img2': '051414-USMNT-30-man-Preliminary-Roster-Hoffenheim-Fabian-Johnson.jpg',
 	 'img2_pos':'20% center',
-	 'caps': 22,
+	 'caps': 23,
 	 'goals': 1,
 	 'number': 23,
 	 'bio':"Son of an American serviceman and a German mother, he played in the Germany youth national team system and started against England in the final of the 2009 European Under-21 Championship. He then switched allegiance to the United States in 2011 and made his debut that November in exhibition at France. Can play either right or left back or in midfield. Has played for 1860 Munich (2006-09), Wolfsburg (2009-11) and Hoffenheim (2011-14), and in February agreed to sign with Borussia Moenchengladbach for next season."
@@ -315,7 +315,7 @@ var usmnt_players = [
 	 'img_pos': 'center center',
 	 'img2': '051314-Soccer-Schalke-Jermaine-Jones.jpg',
 	 'img2_pos':'70% center',
-	 'caps': 42,
+	 'caps': 43,
 	 'goals': 2,
 	 'number': 13,
 	 'bio':"Born in Frankfurt to an American father and German mother, Jones lived in the United States until he was 7 and would visit each year during vacations. Made debut for German national team as a substitute against Austria on Feb. 6, 2008, then appeared against Belarus on May 27 and England on Nov. 19 - all exhibitions. Was among Germany coach Joachim Loew's final roster cuts for the 2008 European Championship, and in 2009 he asked FIFA for a change in affiliation last summer. Made U.S. debut in October 2010. Scored against Jamaica in 2011 CONCACAF Gold Cup and vs. Scotland in 2012 exhibition. Has 72 club yellow cards since the start of the 2005-06 season, according to soccerway.com. Has played for Eintracht Frankfurt (1999-04, 2005-07), Bayer Leverkusen (2004-05), Schalke (2007-14), Blackburn (2011) and Besiktas (2014)."
@@ -379,7 +379,7 @@ var usmnt_players = [
 	 'img_pos': '75% center',
 	 'img2': '051314-Soccer-USA-Graham-Zusi.jpg',
 	 'img2_pos':'45% center',
-	 'caps': 23,
+	 'caps': 24,
 	 'goals': 3,
 	 'number': 19,
 	 'bio':"Scored in January 2012 exhibition at Panama, then had goals in World Cup qualifiers against Jamaica and at Panama last October. Goal vs. Reggae Boyz was in his home stadium, Sporting Park; the goal vs. Panama tied the score in stoppage time, advancing Mexico to a playoff for a World Cup berth at the expense of Panama. Spent entire professional career since 2009 with Sporting Kansas City. Helped Maryland win NCAA titles in 2005 and 2008. Received degree in criminology."
@@ -391,7 +391,7 @@ var usmnt_coaches = [
 	 'ht': '5-11',
 	 'wt': '161',
 	 'birthdate': '7/30/64',
-	 'hometown': 'Göppingen, Germany',
+	 'hometown': 'GÃ¶ppingen, Germany',
 	 'club': 'Germany National Team',
 	 'img': 'JurgenKlinsmannUSMNT1-HondurasHomeWCQ2013-ISIPhotos.jpg',
 	 'img_pos': 'center center',
@@ -400,7 +400,7 @@ var usmnt_coaches = [
 	 'caps': 108,
 	 'goals': 47,
 	 'number': null,
-	 'bio':"A German football manager and former player who is currently the head coach of the United States national team. As a player, Klinsmann played for several prominent clubs in Europe and was part of the West German team that won the 1990 FIFA World Cup and the unified German team that won the 1996 UEFA European Championship. One of Germany's premier strikers during the 1990s, he scored in all six major international tournaments he participated in, from Euro 1988 to 1998 World Cup. In 1995 he came in third in the FIFA World Player of the Year award; in 2004 he was named in the FIFA 100 list of the \"125 Greatest Living Footballers\".  Klinsmann managed the German national team to a third-place finish in the 2006 World Cup. On 12 July 2006, he officially announced that he would step down as Germany's coach after two years in charge and be replaced by assistant coach Joachim Löw. He took over as coach of Bundesliga club Bayern Munich in July 2008 when Ottmar Hitzfeld stepped down. On 27 April 2009, he was released early, even though he had won five of the previous seven league games and was only three points behind league leader VfL Wolfsburg. In the jointly initiated reforms at Bayern, it emerged there was a severe clash of opinions between coach and club management. On 29 July 2011, the US Soccer Federation named Klinsmann the coach of the United States men's national team. In 2013 he won the CONCACAF Gold Cup with the United States. Outside of his professional football life he earned a diploma as a baker in 1982, and is in fact a journeyman baker, having served an apprenticeship. He is married to Debbie Chin, an American former model, and lives in Huntington Beach, California. Klinsmann and his wife have two children, Jonathan and Laila. Klinsmann runs a Children Charity foundation in four countries, is fluent in four languages and one of his hobbies is flying helicopters in Southern California."
+	 'bio':"A German football manager and former player who is currently the head coach of the United States national team. As a player, Klinsmann played for several prominent clubs in Europe and was part of the West German team that won the 1990 FIFA World Cup and the unified German team that won the 1996 UEFA European Championship. One of Germany's premier strikers during the 1990s, he scored in all six major international tournaments he participated in, from Euro 1988 to 1998 World Cup. In 1995 he came in third in the FIFA World Player of the Year award; in 2004 he was named in the FIFA 100 list of the \"125 Greatest Living Footballers\".  Klinsmann managed the German national team to a third-place finish in the 2006 World Cup. On 12 July 2006, he officially announced that he would step down as Germany's coach after two years in charge and be replaced by assistant coach Joachim LÃ¶w. He took over as coach of Bundesliga club Bayern Munich in July 2008 when Ottmar Hitzfeld stepped down. On 27 April 2009, he was released early, even though he had won five of the previous seven league games and was only three points behind league leader VfL Wolfsburg. In the jointly initiated reforms at Bayern, it emerged there was a severe clash of opinions between coach and club management. On 29 July 2011, the US Soccer Federation named Klinsmann the coach of the United States men's national team. In 2013 he won the CONCACAF Gold Cup with the United States. Outside of his professional football life he earned a diploma as a baker in 1982, and is in fact a journeyman baker, having served an apprenticeship. He is married to Debbie Chin, an American former model, and lives in Huntington Beach, California. Klinsmann and his wife have two children, Jonathan and Laila. Klinsmann runs a Children Charity foundation in four countries, is fluent in four languages and one of his hobbies is flying helicopters in Southern California."
 	},
 ];
 /*
